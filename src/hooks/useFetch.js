2e342cdbf394b1af3a4573235804cb535db0effc
yudlinh0810 <yudlinh0810@gmail.com>
2024-10-25 23:01:10 +0700

    create fetchData(hooks) , update Home(pages)

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(endpoint);
+      setLoading(false);
+      setData(response.data.results);
+    } catch (error) {
+      console.log('err', error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [endpoint]);
+
+  return { data, loading };
+};
+export default useFetch;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
 import BannerHome from '../components/BannerHome';
+import { useSelector } from 'react-redux';
+import HorizontalScrollCard from '../components/HorizontalScrollCard';
+import useFetch from '../hooks/useFetch';
 
 const Home = () => {
+  const trendingData = useSelector((state) => state.movieoData.bannerData);
+  const { data: nowPlayingData } = useFetch('/movie/now_playing');
+  const { data: topRateData } = useFetch('/movie/top_rated');
+  const { data: popularTVShowData } = useFetch('/tv/popular');
+  const { data: onAirShowData } = useFetch('/tv/on_the_air');
   return (
     <div>
       <div>
         <BannerHome />
+        <HorizontalScrollCard data={trendingData} heading={'Trending'} trending={true} />
+        <HorizontalScrollCard data={nowPlayingData} heading={'Now Playing'} media_type={'movie'} />
+        <HorizontalScrollCard data={topRateData} heading={'Top Rate Movies'} media_type={'movie'} />
+        <HorizontalScrollCard
+          data={popularTVShowData}
+          heading={'Popular TV Show'}
+          media_type={'tv'}
+        />
+        <HorizontalScrollCard
+          data={onAirShowData}
+          heading={'TV shows that air in the next 7 days'}
+          media_type={'tv'}
+        />
       </div>
     </div>
   );
