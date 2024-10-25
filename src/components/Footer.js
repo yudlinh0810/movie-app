13d855364525ffe778f9cd3e19262d557cdc8027
yudlinh0810 <yudlinh0810@gmail.com>
2024-10-25 23:05:33 +0700

    C_10 complete Footer(component), ExplorePage (pages)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='text-center bg-neutral-600 bg-opacity-40 text-neutral-300 py-3 mt-4'>
+      <div className='flex items-center justify-center gap-6 '>
+        <Link to={'/'} className='hover:text-blue-500'>
+          About
+        </Link>
+        <Link to={'https://github.com/yudlinh0810/movie-app.git'} className='hover:text-blue-500'>
+          Github
+        </Link>
+        <Link to={'/'} className='hover:text-blue-500'>
+          App Movies
+        </Link>
+      </div>
+      <Link
+        to={'https://www.facebook.com/profile.php?id=100013389840204'}
+        className='hover:text-blue-500'
+      >
+        YudLinh
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Card from '../components/Card';
 
 const ExplorePage = () => {
-  return <div>ExplorePage</div>;
+  const param = useParams();
+  const [pageNumber, setPageNumber] = useState(1);
+  const [data, setData] = useState([]);
+  const [totalPageNumber, setTotalPageNumber] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`/discover/${param.explore}`, {
+        params: {
+          page: pageNumber,
+        },
+      });
+      setData((preve) => {
+        return [...preve, ...response.data.results];
+      });
+      setTotalPageNumber(response.data.total_pages);
+      console.log('res', response.data.results);
+    } catch (error) {
+      console.log('err', error);
+    }
+  };
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      setPageNumber((preve) => preve + 1);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [pageNumber]);
+
+  useEffect(() => {
+    setPageNumber(1);
+    setData([]);
+    fetchData();
+  }, [param.explore]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className='pt-[82px]'>
+      <div className='container mx-auto'>
+        <h3 className='capitalize my-3 text-lg font-semibold'>Popular {param.explore}</h3>
+        <div className='grid justify-center grid-cols-[repeat(auto-fit,_240px)] gap-6'>
+          {data.map((exploreData, index) => {
+            return (
+              <Card
+                data={exploreData}
+                key={exploreData.id + 'exploreSection' + index}
+                media_type={param.explore}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExplorePage;
